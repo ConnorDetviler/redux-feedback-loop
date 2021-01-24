6a37937bdb6217b5a13d0948473023c045eb5ffa
Connor Detviler <connor.detviler@gmail.com>
2021-01-23 21:58:42 -0600

    created server POST route

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,9 +3,27 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 const feedbackRouter = express.Router();
 
+// POST
 router.post('/', (req,res) => {
-    const newFeedback = req.body;
+    const feedback = req.body;
+    const queryText =
+    `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments", "flagged", "date")
+    VALUES ($1, $2, $3, $4, $5, date);`;
 
-})
+    pool.query(queryText,
+            [feedback.feeling,
+            feedback.understanding,
+            feedback.support,
+            feedback.comments,
+            false
+            ]
+        ).then((result) => {
+            console.log(result);
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
 
 module.exports = feedbackRouter;
