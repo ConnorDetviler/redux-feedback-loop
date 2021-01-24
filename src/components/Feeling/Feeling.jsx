78f011783feaa553ffddc4f46341581baaedf9b7
Connor Detviler <connor.detviler@gmail.com>
2021-01-24 13:33:42 -0600

    added some form validation in Feeling.jsx

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -15,6 +15,22 @@ function Feeling() {
         history.push('/understanding')
     }
 
+    const handleInput = (event) => {
+        let input = event.target.value;
+        // following conditional allows user to quickly replace number by entering a different one
+        // limits input to one digit
+        if (input.length > 1){
+            input = input.slice(-1);
+        }
+        // next conditional limits that one digit to numbers 1 thru 5
+        if (input > 5) {
+            input = 5;
+        } else if (input < 1) {
+            input = 1;
+        }
+        setFeeling(input)
+    }
+
     return (
         <div>
             <h1>How are you feeling today?</h1>
@@ -24,8 +40,9 @@ function Feeling() {
                     <input
                         type="number"
                         value={feeling}
-                        onChange={(event) => setFeeling(event.target.value)}
+                        onChange={(event) => handleInput(event)}
                         placeholder="1 - 5"
+                        maxLength={1}
                     />
                 </label>
                 <input type="submit" value="Next"/>
