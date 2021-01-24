e1df95e131cae6ee86090a290a441bbc85cd2494
Connor Detviler <connor.detviler@gmail.com>
2021-01-23 21:25:18 -0600

    Review.jsx now displays all feedback

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,8 +1,18 @@
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Review() {
+
+    const feedback = useSelector((state) => state.feedbackReducer)
+
     return (
-        <p>hello from Review</p>
+        <div>
+            <h1>Review your feedback</h1>
+            <h3>Feelings: {feedback.feeling} </h3>
+            <h3>Understanding: {feedback.understanding} </h3>
+            <h3>Support: {feedback.support} </h3>
+            <h3>Comments: {feedback.comments} </h3>
+        </div>
     )
 }
 
